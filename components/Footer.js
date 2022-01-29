7fa7d6e6201081fbb467e1aeba8a89b62a351c1a
ercua <30731448+ercua@users.noreply.github.com>
2022-01-29 14:51:49 +0200

    Add to footer link statrrus

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,9 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <div>
+        [![Netlify Status](https://api.netlify.com/api/v1/badges/8eba1aa4-5386-4df0-9eae-7c6741be3072/deploy-status)](https://app.netlify.com/sites/lucid-lumiere-941437/deploys)
+        </div>
       </footer>
     </>
   )
